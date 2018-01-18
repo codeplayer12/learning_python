687c7068c5c8d01f145930949f02e0b3eea703eb
Noble Mutoko <noblemutoko@gmail.com>
2018-01-18 18:59:47 +0200

    added inheritance function

--- car.py
+++ car.py
@@ -23,19 +23,28 @@ class Car():
         Add the given amount to the odometer reading.
         Add control to disallow negative values from being entered.
         """
-        if mile < 0:
+        if miles < 0:
             print("You cannot ente negative values")
         else:
             self.odometer_reading += miles
 
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name."""
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        long_name = str(self.year) + '\t' + self.make + '\t' + self.model
         return long_name.title()
 
     def read_odometer(self):
         """Print a statement showing the car's mileage."""
         print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+class ElectricCar(Car):
+    """represent aspects of a car, specific to electric vehicles."""
+    def __init__(self,make,model,year):
+        super().__init__(make,model,year)
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
