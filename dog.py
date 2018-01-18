ddd02af1bd042ef69327ebe6724b4f1403f8ccb6
noble mutoko <33986874+codeplayer12@users.noreply.github.com>
2018-01-18 12:08:19 +0200

    Add files via upload

---
+++ dog.py
@@ -0,0 +1,15 @@
+class Dog():
+    """A simple attempt to model a dog."""
+
+    def __init__(self,name,age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in a response to a command."""
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        """Simulate rolling over in a response to a command."""
+        print(self.name.title() + " rolled over!")
